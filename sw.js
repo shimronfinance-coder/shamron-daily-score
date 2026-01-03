86ac32ac57a9915b08e5406872dd4e679567db4b
shimronfinance-coder <shimronfinance@gmail.com>
2026-01-03 23:52:56 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "shamron-daily-score-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
